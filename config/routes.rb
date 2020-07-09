f6e7885025b6ef522bdff633a914da1b1a9a8ff1
Ryan William Tierney <ryan.tierney92@gmail.com>
2020-07-09 01:52:45 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -54,6 +54,6 @@ Rails.application.routes.draw do
   #     resources :products
   #   end
   
-  resources :coupons, only [:index, :new, :create]
+  resources :coupons, only: [:index, :new, :create]
   get 'coupon/:id', to: 'coupons#show' as: 'coupon'
 end
